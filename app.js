a36cd1caeff8f07aa0924b9ac436ca0d120caac4
Assyl99 <salaha@student.mini.pw.edu.pl>
2020-08-07 23:43:04 +0200

    File Upload

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 var session = require('express-session');
@@ -59,6 +60,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
